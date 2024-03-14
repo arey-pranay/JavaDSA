ba641c917b8f3d5290362b75946b7d54ab4c0729
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-03-15 00:24:49 +0530

    Update 14. PowX,N-#50.java

--- Array-1/Pranay/14. PowX,N-#50.java
+++ Array-1/Pranay/14. PowX,N-#50.java
@@ -1,20 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        
+        //for negetive powers, turn x ro 1/x and make n positive.
         if(n < 0){
             n = -n;
             x = 1 / x;
         }
         
         double pow = 1;
-        
+        //Binary Exponentiation
         while(n != 0){
+            //check if the last digit (lsb) is 1, if it is then you multiply your answer with the current val of x.And the right shift to update the last digit of n.
             if((n & 1) != 0){
+                //include that x^something in your answer
                 pow *= x;
             } 
-                
+            //x becomes x2 then x4 then x8 then x16...    
             x *= x;
-            n=n/2;
+            //right shift n, to eliminate the old last digit and update it with the second last digit
+            n=n>>2;
             
         }
         
