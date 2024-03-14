30921fb592a6a660034331a3c0b51cfa7c752661
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-03-15 00:00:06 +0530

    Update 13. SearchIn2DMatrix-#74.java

--- Array-1/Pranay/13. SearchIn2DMatrix-#74.java
+++ Array-1/Pranay/13. SearchIn2DMatrix-#74.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int cols = matrix[0].length;
+        
+        //if there is only one array then apply binarySearch directly on that
         if (matrix.length == 1) {
-            if (Arrays.binarySearch(matrix[0], target) >= 0) return true;
+            if (Arrays.binarySearch(matrix[0], target) >= 0) return true; 
             return false;
         }
+        //find the row where the element is present
         int[] rowFound = findRow(matrix, target, 0, matrix.length - 1);
+        //apply binarySearch in that row, if found, return true
         if (rowFound != null && Arrays.binarySearch(rowFound, target) >= 0) return true;
         return false;
     }
 
     public static int[] findRow(int[][] mat, int t, int s, int e) {
+        //it takes the matrix, target, start and end
         int cols = mat[0].length;
         if (s > e) return null; // Base case: return null if the search space is exhausted
+        //now decide which row to start you search from and end your search at, and apply a binarySearch sorta technique on that
         int mid = s + (e - s) / 2;
         int[] row = mat[mid];
         int temp1 = row[0];
