c4958c567481048264c08c79208464ce083eacdb
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-03-15 20:40:08 +0530

    Create 20. TwoSum-#1.java

---
+++ Array-1/Pranay/20. TwoSum-#1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       for(int i=1;i<nums.length;i++)
+            {
+                for(int j=i;j<nums.length;j++)
+                {
+                    if(nums[j-i]+nums[j]==target)
+                    {
+                        return new int[]{j-i,j};
+                    }
+                }
+            }
+        
+        return new int[]{};
+    }
+}
