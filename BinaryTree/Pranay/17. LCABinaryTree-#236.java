c9ebc77e1916649aac8d542e44770af5f672b736
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-04-06 19:26:54 +0530

    Create 17. LCABinaryTree-#236.java

---
+++ BinaryTree/Pranay/17. LCABinaryTree-#236.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == q || root == p) {
+            return root;
+        }
+      
+        TreeNode l = lowestCommonAncestor( root.left,  p,  q);
+        TreeNode r = lowestCommonAncestor( root.right,  p,  q);
+        if(l!= null && r!=null) {
+            return root;
+        }
+        if(l!=null) {
+            return l;
+        }
+        return r;
+    }
+}
