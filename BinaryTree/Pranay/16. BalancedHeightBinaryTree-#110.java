632dc3cd39e4411e300a2e6e1df435db19651f9c
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-04-15 19:52:17 +0530

    Create 16. BalancedHeightBinaryTree-#110.java

---
+++ BinaryTree/Pranay/16. BalancedHeightBinaryTree-#110.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return func(root) != -1;
+    }
+    public int func(TreeNode root){
+        if(root==null) return 0;
+        int leftHeight = func(root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = func(root.right);
+        if (rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+        return Math.max(leftHeight,rightHeight)+1;
+    }
+}
