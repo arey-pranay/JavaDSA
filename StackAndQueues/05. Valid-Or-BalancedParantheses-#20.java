a58453e3653284a5759166f5b1dafe66646fa8fc
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-04-20 15:04:10 +0530

    Create 05. Valid-Or-BalancedParantheses-#20.java

---
+++ StackAndQueues/05. Valid-Or-BalancedParantheses-#20.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>(); // create an empty stack
+        for (char c : s.toCharArray()) { // loop through each character in the string
+            if (c == '(') // if the character is an opening parenthesis
+                stack.push(')'); // push the corresponding closing parenthesis onto the stack
+            else if (c == '{') // if the character is an opening brace
+                stack.push('}'); // push the corresponding closing brace onto the stack
+            else if (c == '[') // if the character is an opening bracket
+                stack.push(']'); // push the corresponding closing bracket onto the stack
+            else if (stack.isEmpty() || stack.pop() != c) // if the character is a closing bracket
+                // if the stack is empty (i.e., there is no matching opening bracket) or the top of the stack
+                // does not match the closing bracket, the string is not valid, so return false
+                return false;
+        }
+        // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
+        // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+        return stack.isEmpty();
+    }
+}
