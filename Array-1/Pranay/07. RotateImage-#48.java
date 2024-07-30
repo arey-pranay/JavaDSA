7c8661aeea21e4902e561ff8d8bff872d1fe8c92
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-07-30 18:38:35 +0530

    Update 07. RotateImage-#48.java

--- Array-1/Pranay/07. RotateImage-#48.java
+++ Array-1/Pranay/07. RotateImage-#48.java
@@ -21,3 +21,34 @@ class Solution {
         }
     }
 }
+
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         // transpose the matrix, diagonally
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=i;j<matrix[0].length;j++){
+//                 swapMat(matrix,i,j);
+//             }
+//         }
+//         for(int[] a : matrix) revRow(a);
+//     }
+//     public void swapMat (int[][] mat, int i, int j){
+//         int temp = mat[i][j];
+//         mat[i][j] = mat[j][i];
+//         mat[j][i] = temp;
+//     }
+//     public void revRow (int[] row){
+//         int i=0;
+//         int j= row.length-1;
+//         while(i<=j){
+//             swap(row,i,j);
+//             i++;
+//             j--;
+//         }
+//     }
+//     public void swap (int[] arr, int i, int j){
+//         int temp  = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//     }
+// }
