e15beee3348f396808c3a1c512adb1c4f062bd4f
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-02 18:24:04 +0530

    Update 19. TwoSum-#1.java

--- Array-1/Pranay/19. TwoSum-#1.java
+++ Array-1/Pranay/19. TwoSum-#1.java
@@ -1,16 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       for(int i=1;i<nums.length;i++)
-            {
-                for(int j=i;j<nums.length;j++)
-                {
-                    if(nums[j-i]+nums[j]==target)
-                    {
-                        return new int[]{j-i,j};
-                    }
-                }
-            }
-        
-        return new int[]{};
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])) return new int[]{hm.get(target-nums[i]),i};
+            hm.put(nums[i],i);
+        }
+        return new int[] {-1,-1};
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//        for(int i=1;i<nums.length;i++)
+//             {
+//                 for(int j=i;j<nums.length;j++)
+//                 {
+//                     if(nums[j-i]+nums[j]==target)
+//                     {
+//                         return new int[]{j-i,j};
+//                     }
+//                 }
+//             }
+        
+//         return new int[]{};
+//     }
+// }
