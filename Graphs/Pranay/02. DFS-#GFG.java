a786e32cfa0b8bf95b830cbe8245155f2b2c54c5
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-05 10:56:48 +0530

    Update 02. DFS-#GFG.java

--- Graphs/Pranay/02. DFS-#GFG.java
+++ Graphs/Pranay/02. DFS-#GFG.java
@@ -1,27 +1,56 @@
-
 class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean vis[] = new boolean[V];
-        vis[0] = true;
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        ans.add(0);
-        dfs(0,vis,ans,V,adj);
+        // Code here
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        // ans.add(0);
+        boolean[] visited = new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                ans.add(i);
+                dfs(i,adj,ans, visited);
+            }
+        }
+        
         return ans;
     }
-    public void dfs(int index, boolean[] vis, ArrayList<Integer> ans, int max,ArrayList<ArrayList<Integer>> adj){
-        if(ans.size()==max) return;
-        for(int j : adj.get(index)){
-                if(!vis[j]){
-                    vis[j] = true;
-                    ans.add(j);
-                    dfs(j,vis,ans,max,adj);
-                }
-                
+    public void dfs(int curr, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ans, boolean[] vis){
+        for(int i: adj.get(curr)){
+            if(!vis[i]){
+                vis[i] = true;
+                ans.add(i);
+                dfs(i,adj,ans,vis);
+            }
         }
-        return;
     }
 }
+
+
+// class Solution {
+//     // Function to return a list containing the DFS traversal of the graph.
+//     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+//         boolean vis[] = new boolean[V];
+//         vis[0] = true;
+//         ArrayList<Integer> ans = new ArrayList<Integer>();
+//         ans.add(0);
+//         dfs(0,vis,ans,V,adj);
+//         return ans;
+//     }
+//     public void dfs(int index, boolean[] vis, ArrayList<Integer> ans, int max,ArrayList<ArrayList<Integer>> adj){
+//         if(ans.size()==max) return;
+//         for(int j : adj.get(index)){
+//                 if(!vis[j]){
+//                     vis[j] = true;
+//                     ans.add(j);
+//                     dfs(j,vis,ans,max,adj);
+//                 }
+                
+//         }
+//         return;
+//     }
+// }
 
 // class Solution {
 //     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
