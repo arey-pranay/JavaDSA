9110f738d0893c4c1f547233ad5a2eba288e863d
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-07 17:05:50 +0530

    Update 02. MiddleOfALinkedList-#876.java

--- LinkedList/Pranay/02. MiddleOfALinkedList-#876.java
+++ LinkedList/Pranay/02. MiddleOfALinkedList-#876.java
@@ -12,19 +12,30 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        //jbtk fast end pr nhi pohcha
-        while(fast != null){
-            if(fast.next==null) {//odd numbers 
-               return slow;
-            } else if(fast.next.next == null){ //even number
-                return slow.next;
-            }
-            else {
-                fast = fast.next.next; //fast ek baar me 2 steps chlta hai
-                slow = slow.next; //slow ek baar me ek step chlta hai
-            }
-            //jb fast end pe pohch jayega tb slow mid pe hoga
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
         return slow;
     }
 }
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode fast = head;
+//         ListNode slow = head;
+//         //jbtk fast end pr nhi pohcha
+//         while(fast != null){
+//             if(fast.next==null) {//odd numbers 
+//                return slow;
+//             } else if(fast.next.next == null){ //even number
+//                 return slow.next;
+//             }
+//             else {
+//                 fast = fast.next.next; //fast ek baar me 2 steps chlta hai
+//                 slow = slow.next; //slow ek baar me ek step chlta hai
+//             }
+//             //jb fast end pe pohch jayega tb slow mid pe hoga
+//         }
+//         return slow;
+//     }
+// }
