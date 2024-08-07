5d885ea7e8d3e6314fb5d4eb0a7b100ca1953c11
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-07 18:27:10 +0530

    Update 06. DeleteAGivernNode-#237.java

--- LinkedList/Pranay/06. DeleteAGivernNode-#237.java
+++ LinkedList/Pranay/06. DeleteAGivernNode-#237.java
@@ -6,16 +6,23 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
 class Solution {
     public void deleteNode(ListNode node) {
-        if (node == null || node.next == null) {
-            // If the node or its next node is null, we cannot delete it
-            return;
-        }
-        
-        // Copy the value of the next node to the current node
         node.val = node.next.val;
-        // Point the current node to the node after the next node, effectively removing the next node
         node.next = node.next.next;
     }
 }
+// class Solution {
+//     public void deleteNode(ListNode node) {
+//         if (node == null || node.next == null) {
+//             // If the node or its next node is null, we cannot delete it
+//             return;
+//         }
+        
+//         // Copy the value of the next node to the current node
+//         node.val = node.next.val;
+//         // Point the current node to the node after the next node, effectively removing the next node
+//         node.next = node.next.next;
+//     }
+// }
