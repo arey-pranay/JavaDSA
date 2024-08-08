4e854e1b0a844e0cf5a79f1ae9de600af3105948
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-08 17:40:14 +0530

    Update 07. IntersectionOfTwoLinkedLists-#160.java

--- LinkedList/Pranay/07. IntersectionOfTwoLinkedLists-#160.java
+++ LinkedList/Pranay/07. IntersectionOfTwoLinkedLists-#160.java
@@ -47,3 +47,28 @@ public class Solution {
 
     }
 }
+
+// public class Solution {
+
+// public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//     //boundary check
+//     if(headA == null || headB == null) return null;
+    
+//     ListNode a = headA;
+//     ListNode b = headB;
+    
+//     //if a & b have different len, then we will stop the loop after second iteration
+//     while(a != b){
+//     	//for the end of first iteration, we just reset the pointer to the head of another linkedlist    
+//         // if(a!=null && b!=null) System.out.println(a.val + " " + b.val); 
+//         if(a==null) a = headB;
+//         else a = a.next;
+//         if(b==null) b = headA;
+//         else b = b.next;
+//         // a = a == null? headB : a.next;
+//         // b = b == null? headA : b.next;   
+//     }
+    
+//     return a;
+// }
+// }
