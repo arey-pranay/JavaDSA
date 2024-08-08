e3f6c30988e937cabd2ac1f7ecdfb806192cde2e
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-08 18:52:58 +0530

    Update 10. CheckPalindrome-#234.java

--- LinkedList/Pranay/10. CheckPalindrome-#234.java
+++ LinkedList/Pranay/10. CheckPalindrome-#234.java
@@ -8,6 +8,16 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; } // Constructor
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val; // Node ka value
+ *     ListNode next; // Agla node
+ *     ListNode() {} // Constructor
+ *     ListNode(int val) { this.val = val; } // Constructor
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; } // Constructor
+ * }
+ */
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode fast = head; // Fast pointer jo 2 steps aage badhta hai
@@ -44,3 +54,40 @@ class Solution {
         return true; // Agar sab match hai, toh palindrome hai
     }
 }
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         ListNode fast = head; // Fast pointer jo 2 steps aage badhta hai
+//         ListNode slow = head; // Slow pointer jo 1 step aage badhta hai
+//         ListNode reversed = null; // Reverse kiya hua linked list
+        
+//         // Linked list ke beech tak fast aur slow pointers ko move karo
+//         // Aur reverse karte jao slow pointer ke peeche
+//         while (fast != null && fast.next != null) {
+//             fast = fast.next.next;
+//             ListNode next = slow.next;
+//             slow.next = reversed;
+//             reversed = slow;
+//             slow = next;
+//         }
+        
+//         // Agar fast pointer aage hai toh slow ko ek step aage badhao
+//         if (fast != null) {
+//             slow = slow.next;
+//         }
+        
+//         // Ab slow aur reversed pointers ko compare karo
+//         while (slow != null) {
+//             if (slow.val != reversed.val) { // Agar value match nahi karti, toh palindrome nahi hai
+//                 System.gc(); // Garbage collector ko call karo
+//                 return false;
+//             }
+
+//             slow = slow.next; // Next nodes par move karo
+//             reversed = reversed.next; // Next nodes par move karo
+//         }
+
+//         System.gc(); // Garbage collector ko call karo
+//         return true; // Agar sab match hai, toh palindrome hai
+//     }
+// }
