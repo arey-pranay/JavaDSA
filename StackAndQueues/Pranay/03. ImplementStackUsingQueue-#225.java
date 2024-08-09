22fe7e0dffea8c666051400146894101875c12bd
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-09 19:03:36 +0530

    Update 03. ImplementStackUsingQueue-#225.java

--- StackAndQueues/Pranay/03. ImplementStackUsingQueue-#225.java
+++ StackAndQueues/Pranay/03. ImplementStackUsingQueue-#225.java
@@ -1,26 +1,66 @@
-public class MyStack {
-    private Queue<Integer> q;
+// public class MyStack {
+//     private Queue<Integer> q;
 
+//     public MyStack() {
+//         q = new LinkedList<>();
+//     }
+
+//     public void push(int x) {
+//         q.add(x);
+//         for (int i = 1; i < q.size(); i++) {
+//             q.add(q.remove());
+//         }
+//     }
+
+//     public int pop() {
+//         return q.remove();
+//     }
+
+//     public int top() {
+//         return q.peek();
+//     }
+
+//     public boolean empty() {
+//         return q.isEmpty();
+//     }
+// }
+
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
     public MyStack() {
-        q = new LinkedList<>();
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
-
-    public void push(int x) {
-        q.add(x);
-        for (int i = 1; i < q.size(); i++) {
-            q.add(q.remove());
-        }
+    
+    public void push(int x) {        
+            while(!q1.isEmpty()){
+                q2.add(q1.remove());
+            }
+            q1.add(x);
+            while(!q2.isEmpty()){
+                q1.add(q2.remove());
+            }
     }
-
+    
     public int pop() {
-        return q.remove();
+        return q1.remove();
     }
-
+    
     public int top() {
-        return q.peek();
+        return q1.peek();
     }
-
+    
     public boolean empty() {
-        return q.isEmpty();
+        return q1.isEmpty();
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
