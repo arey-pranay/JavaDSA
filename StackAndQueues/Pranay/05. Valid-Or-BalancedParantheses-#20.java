c9da64f4672f347f2168284860ddf9069d96c17d
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-09 19:18:39 +0530

    Update 05. Valid-Or-BalancedParantheses-#20.java

--- StackAndQueues/Pranay/05. Valid-Or-BalancedParantheses-#20.java
+++ StackAndQueues/Pranay/05. Valid-Or-BalancedParantheses-#20.java
@@ -18,21 +18,60 @@
 //         return stack.isEmpty();
 //     }
 // }
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+//         for(char c : s.toCharArray()){
+//             if(c == ')'){
+//                 if(st.size()==0 || st.pop()!='(') return false;
+//             } else if(c == '}'){
+//                 if(st.size()==0 || st.pop()!='{') return false;
+//             } else if(c == ']'){
+//                 if(st.size()==0 || st.pop()!='[') return false;
+//             } else{
+//                 st.push(c);
+//             }
+//         }
+//         if(st.isEmpty()) return true;
+//         return false;
+//     }
+// }
+
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+
+//         for(char c : s.toCharArray()){
+//             if(c == ')'){
+//                 if(st.size()==0 || st.pop()!='(') return false;
+//             } else if(c == '}'){
+//                 if(st.size()==0 || st.pop()!='{') return false;
+//             } else if(c == ']'){
+//                 if(st.size()==0 || st.pop()!='[') return false;
+//             } else{
+//                 st.push(c);
+//             }
+//         }
+
+//         return st.isEmpty();
+//     }
+// }
 class Solution {
     public boolean isValid(String s) {
+        //agr opening mila to uska corresponding closing stack me dalo
+        //agr closing mila to stack ke top se compare krlo
         Stack<Character> st = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c == ')'){
-                if(st.size()==0 || st.pop()!='(') return false;
-            } else if(c == '}'){
-                if(st.size()==0 || st.pop()!='{') return false;
-            } else if(c == ']'){
-                if(st.size()==0 || st.pop()!='[') return false;
-            } else{
-                st.push(c);
+        char[] cArr = s.toCharArray();
+        for(char c: cArr){
+            if(c=='(') st.push(')');
+            else if(c=='{') st.push('}');
+            else if(c=='[') st.push(']');
+            else {
+                if(st.size()==0) return false;
+                if(c!=st.pop()) return false;
             }
         }
-        if(st.isEmpty()) return true;
-        return false;
+        if(st.size()!=0) return false;
+        return true;
     }
 }
