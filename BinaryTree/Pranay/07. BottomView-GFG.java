2cdeb1a25be1188a09052ca93f1ba4e0320706c5
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-12 19:07:36 +0530

    Update 07. BottomView-GFG.java

--- BinaryTree/Pranay/07. BottomView-GFG.java
+++ BinaryTree/Pranay/07. BottomView-GFG.java
@@ -36,3 +36,32 @@ class Solution
         return ans;
     }
 }
+
+// class Solution{
+//     public ArrayList <Integer> bottomView(Node root){
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         Queue<Pair> q = new LinkedList<>();
+//         TreeMap<Integer, Integer> hm = new TreeMap<>(); // col : node
+//         q.add(new Pair(root,0));
+//         while(!q.isEmpty()){
+//             int sz = q.size();
+//             for(int i=0;i<sz;i++){
+//                 Pair temp = q.remove();
+//                 Node tempNode = temp.n;
+//                 hm.put(temp.col,tempNode.data);
+//                 if(tempNode.left!=null)q.add(new Pair(tempNode.left, temp.col-1));
+//                 if(tempNode.right!=null)q.add(new Pair(tempNode.right,temp.col+1));
+//             }
+//         }
+//         for(Map.Entry<Integer,Integer> e : hm.entrySet())  arr.add(e.getValue());
+//         return arr;
+//     }
+//     class Pair{
+//         Node n;
+//         int col;
+//         Pair(Node n, int col){
+//             this.n = n;
+//             this.col = col;
+//         }
+//     }
+// }
