59d71a3612cbae24a1bb66e054e38f26c6fba8f7
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-12 23:22:49 +0530

    Update 10. VerticalOrderTraversal-#987.java

--- BinaryTree/Pranay/10. VerticalOrderTraversal-#987.java
+++ BinaryTree/Pranay/10. VerticalOrderTraversal-#987.java
@@ -51,3 +51,43 @@ class Solution {
         return a;
     }
 }
+
+// class Solution {
+//     public List<List<Integer>> verticalTraversal(TreeNode root) {
+//         //a hashmap (treemap) to store the list of all elements acc to col (ascending columns)
+//         TreeMap<Integer,ArrayList<Integer>> hm = new TreeMap<>();
+//         //a custom class / ds, to store each node with column number
+//         Pair start = new Pair(0,root);
+//         //a queue for level order traversal
+//         Queue<Pair> q = new LinkedList<>();
+//         q.add(start);
+//         //let us start the level order traversal
+//         while(!q.isEmpty()){
+//             //store size because it'll change on adding more elements and removing them in the loop itself
+//             int sz = q.size();
+//             for(int i=0;i<sz;i++){
+//                 Pair temp = q.remove();
+//                 ArrayList<Integer> updatedList =  hm.getOrDefault(temp.col, new ArrayList<Integer>());
+//                 updatedList.add(temp.n.val); 
+//                 System.out.println(updatedList);
+//                 hm.put(temp.col,updatedList);
+//                 if(temp.n.left!=null) q.offer(new Pair(temp.col-1,temp.n.left));
+//                 if(temp.n.right!=null) q.offer(new Pair(temp.col+1,temp.n.right));
+//             }
+//         }
+//         List<List<Integer>> ans = new ArrayList<>();
+//         for(Map.Entry<Integer,ArrayList<Integer>> e : hm.entrySet()){
+//             System.out.println(e.getKey());
+//             ans.add(e.getValue());
+//         }
+//         return ans;
+//     }
+//     public class Pair{
+//         int col;
+//         TreeNode n;
+//         Pair(int col, TreeNode node){
+//             this.col = col;
+//             this.n = node;
+//         }
+//     }
+// }
